ea7e76f29ee9516413956aebcc7a75e9d3c6a0db
Tony Gaskell <thgaskell@gmail.com>
2014-07-04 18:46:14 -1000

    Add audio back into tab

--- extension/global-chrome.js
+++ extension/global-chrome.js
@@ -1,16 +1,5 @@
-
-chrome.browserAction.onClicked.addListener( function (tab) {
-  chrome.browserAction.setIcon({ tabId : tab.id, path: 'recording.png' });
-});
-
-
 /* Feature detection
  */
-
-function hasGetUserMedia() {
-  return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia || navigator.msGetUserMedia);
-}
 
 var currentTab;
 
@@ -69,4 +58,34 @@ chrome.tabCapture.capture(options, function(stream) {
   // }, error);
 
   audioStream = stream;
+});
+
+chrome.browserAction.onClicked.addListener( function (tab) {
+  chrome.browserAction.setIcon({ tabId : tab.id, path: 'recording.png' });
+  chrome.tabCapture.capture(options, function(stream) {
+    console.log('capturing the audio stream.');
+    // pc.onaddstream(stream);
+    // var socket = io.connect('http://localhost:3000');
+    // socket.on('news', function(data) {
+    //   console.log(data);
+    //   socket.emit('my other event', {my: stream});
+    // });
+
+    var $audio = document.createElement('audio');
+    $audio.src = window.URL.createObjectURL(stream);
+    setTimeout(function(){$audio.play();}, 2000);
+    document.body.appendChild($audio);
+
+    // Adding a local stream won't trigger the onaddstream callback
+    // pc.addStream(stream);
+    // pc.createOffer(function(offer){
+    //   console.log('creating the offer');
+    //   pc.setLocalDescription(new RTCSessionDescription(offer), function() {
+    //     // send the offer to a server to be forwarded to the friend you're calling.
+    //     console.log('sending the offer!');
+    //   }, error);
+    // }, error);
+
+    audioStream = stream;
+  });
 });
