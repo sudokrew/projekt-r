4450527aafcb70db55f62406388beb0d18c0a148
Tony Gaskell <thgaskell@gmail.com>
2014-07-04 19:22:56 -1000

    Add stream toggle

--- extension/global-chrome.js
+++ extension/global-chrome.js
@@ -31,37 +31,11 @@ function error(err) {
   endCall();
 }
 
-// Get a list of friends from a server
-// User selects a friend to start a peer connection with
-chrome.tabCapture.capture(options, function(stream) {
-  console.log('capturing the audio stream.');
-  // pc.onaddstream(stream);
-  var socket = io.connect('http://localhost:3000');
-  socket.on('news', function(data) {
-    console.log(data);
-    socket.emit('my other event', {my: stream});
-  });
+var $audio = document.createElement('audio');
 
-  var $audio = document.createElement('audio');
-  $audio.src = window.URL.createObjectURL(stream);
-  setTimeout(function(){$audio.play();}, 2000);
-  document.body.appendChild($audio);
-
-  // Adding a local stream won't trigger the onaddstream callback
-  // pc.addStream(stream);
-  // pc.createOffer(function(offer){
-  //   console.log('creating the offer');
-  //   pc.setLocalDescription(new RTCSessionDescription(offer), function() {
-  //     // send the offer to a server to be forwarded to the friend you're calling.
-  //     console.log('sending the offer!');
-  //   }, error);
-  // }, error);
-
-  audioStream = stream;
-});
-
-chrome.browserAction.onClicked.addListener( function (tab) {
-  chrome.browserAction.setIcon({ tabId : tab.id, path: 'recording.png' });
+function startStream() {
+  // Get a list of friends from a server
+  // User selects a friend to start a peer connection with
   chrome.tabCapture.capture(options, function(stream) {
     console.log('capturing the audio stream.');
     // pc.onaddstream(stream);
@@ -71,9 +45,8 @@ chrome.browserAction.onClicked.addListener( function (tab) {
     //   socket.emit('my other event', {my: stream});
     // });
 
-    var $audio = document.createElement('audio');
     $audio.src = window.URL.createObjectURL(stream);
-    setTimeout(function(){$audio.play();}, 2000);
+    $audio.play();
     document.body.appendChild($audio);
 
     // Adding a local stream won't trigger the onaddstream callback
@@ -88,4 +61,21 @@ chrome.browserAction.onClicked.addListener( function (tab) {
 
     audioStream = stream;
   });
+}
+
+function stopStream() {
+  audioStream.stop();
+  $audio.pause();
+}
+
+chrome.browserAction.onClicked.addListener( function (tab) {
+  if ($audio.paused) {
+    startStream();
+    chrome.browserAction.setIcon({ tabId : tab.id, path: 'recording.png' });
+  }
+  else {
+    stopStream();
+    chrome.browserAction.setIcon({ tabId : tab.id, path: 'icon.png' });
+  }
+
 });
